bdcefee8c916a1640894ca0e8ad0c541fb87bc6f
abfo_cp <SND\abfo_cp>
2010-06-20 23:42:36 +0000

    First Version

---
+++ Blogger2BlogML/ConverterMessageEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blogger2BlogML
+{
+    /// <summary>
+    /// A message to display or log
+    /// </summary>
+    public class ConverterMessageEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The message
+        /// </summary>
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// A message to display or log
+        /// </summary>
+        /// <param name="message">The message</param>
+        public ConverterMessageEventArgs(string message)
+        {
+            if (message == null) { throw new ArgumentNullException("message"); }
+            this.Message = message;
+        }
+    }
+}
---
+++ Blogger2BlogML/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Blogger2BlogML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print banner
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
+            Console.WriteLine(Properties.Resources.Program_Banner, fvi.ProductMajorPart, fvi.ProductMinorPart, fvi.LegalCopyright);
+            Console.WriteLine();
+
+            if (args.Length == 2)
+            {
+                try
+                {
+                    BlogConverter converter = new BlogConverter(args[0], args[1]);
+                    converter.ConverterMessage += new EventHandler<ConverterMessageEventArgs>(converter_ConverterMessage);
+                    converter.Convert();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(Properties.Resources.Program_Exception, ex);
+                    Console.WriteLine();
+                    Console.WriteLine(Properties.Resources.Program_ReportError);
+
+                    Environment.ExitCode = 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine(Properties.Resources.Program_Usage);
+                Environment.ExitCode = 2;
+            }
+#if DEBUG
+            Console.ReadKey();
+#endif
+            Console.WriteLine();
+        }
+
+        static void converter_ConverterMessage(object sender, ConverterMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
